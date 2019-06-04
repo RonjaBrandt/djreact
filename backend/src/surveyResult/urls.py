17e4b5124683d21cf712e349a3906f0686b8dbc6
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-06-04 22:12:43 +0200

    Added view for detalies

--- backend/src/surveyResult/urls.py
+++ backend/src/surveyResult/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls import url
 from . import views
 
 # Handel url request, to the site.
 
 urlpatterns = [
-    # ex: /surveyResult/
+    # /surveyResult/
     path('', views.index, name='index'),
+
+    # /surveyResult/123  (id)
+    re_path(r'^(?P<survey_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- backend/src/surveyResult/views.py
+++ backend/src/surveyResult/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     #Sendsback the respons.
     return HttpResponse("This is the result homepage")
+
+def detail(request, survey_id):
+    return HttpResponse("Details for the Answers with id: " + str(survey_id) + ".")
