5b18394e7d6890116e02508528fde012edb7c7e1
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-06-04 18:41:18 +0200

    Delete path to api from before

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 # Handel url request, to the site.
 
 urlpatterns = [
-   # path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-   # path('api/', include('articles.api.urls'))
+
 ]
--- backend/src/surveyResult/models.py
+++ backend/src/surveyResult/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+#Survey
+#1 Survey to many Questions
+#Question
+#1 Question to 1 answer
+#Answer
