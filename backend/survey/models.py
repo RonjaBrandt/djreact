59029e4fd604320be0c53aaf6daa48323797b53e
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-06-08 15:04:35 +0200

    Updated views and reander

--- backend/survey/models.py
+++ backend/survey/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Survey(models.Model):
    # Takes in all the data
+   title = models.CharField(max_length=100)
    items = models.CharField(max_length=500)
 
    def __str__(self):
@@ -11,3 +12,6 @@ class Survey(models.Model):
 class Questions(models.Model):
     items = models.ForeignKey(Survey, on_delete=models.CASCADE)
     question_answers = models.CharField(max_length=500)
+
+    def __str__(self):
+       return '----BEGINING---- ' + self.question_answers + ' ---- END-----'
--- backend/survey/views.py
+++ backend/survey/views.py
@@ -4,8 +4,11 @@ from .models import Survey
 
 def index(request):
     all_survey = Survey.objects.all()
-    template = loader.get_template('')
-    return HttpResponse()
+    template = loader.get_template('survey/index.html')
+    context = {
+        'all_survey': all_survey,
+    } 
+    return HttpResponse(template.render(context, request))
 
 def detail(request, survey_id):
     return HttpResponse("<h2>This is the DETAILVIEW for survey with id: " 
