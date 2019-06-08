9b99f32e5eb47aee7bf55ba340bd3115f6f242cd
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-06-08 13:46:45 +0200

    Update admin

--- backend/survey/models.py
+++ backend/survey/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+   # Takes in all the data
+   items = models.CharField(max_length=500)
+
+   def __str__(self):
+       return '----BEGINING---- ' + self.items + ' ---- END-----'
+
+class Questions(models.Model):
+    items = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_answers = models.CharField(max_length=500)
--- backend/survey/urls.py
+++ backend/survey/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /survey/
     path('', views.index, name='index'),
+
+    # /survey/6214/
+    path(r'^(?P<survey_id>[')
 ]
