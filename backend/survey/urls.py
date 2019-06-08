21ce58dd568deca861e62c8ccd7e4d3050d303e7
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-06-08 13:52:33 +0200

    Added detail views start

--- backend/survey/urls.py
+++ backend/survey/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     # /survey/
     path('', views.index, name='index'),
 
-    # /survey/6214/
-    path(r'^(?P<survey_id>[')
+    # /survey/6214/  <-that is  (id)
+    re_path(r'^(?P<survey_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- backend/survey/views.py
+++ backend/survey/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the Survey app homepage</h1>")
+
+def detail(request, survey_id):
+    return HttpResponse("<h2>This is the DETAILVIEW for survey with id: " 
+    + str(survey_id + "</h2>"))
