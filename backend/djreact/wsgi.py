78d8e9e05d60469b2445283cbfeb01532ee972f9
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-06-25 14:59:33 +0200

    Update buildout

--- backend/djreact/wsgi.py
+++ backend/djreact/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for src project.
+WSGI config for djreact project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djreact.settings')
 
 application = get_wsgi_application()
