47f40401c5e4f2b96b1c69aab03712e3ed5b169e
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-07-29 11:50:20 +0200

    update

--- backend/survey/models.py
+++ backend/survey/models.py
@@ -2,47 +2,43 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import Max
 
+
 class Survey(models.Model):
-   survey_Id = models.CharField(max_length=20, help_text="Add the Survey ID from Typefrom here")
-   
-   def __str__(self):
-      return 'SurveyID: '+self.survey_Id
-   # TODO: Skapa Read-only fält för current points
+    survey_Id = models.CharField(max_length=20, help_text="Add the Survey ID from Typefrom here")
+
+    def __str__(self):
+        return 'SurveyID: '+self.survey_Id
+    # TODO: Skapa Read-only fält för current points
+
+
 class Category(models.Model):
     survey = models.ForeignKey(Survey, related_name='category', on_delete=models.CASCADE, help_text="Choose what Survey this Category belongs to.", blank=True, null=True)
     category_Name = models.CharField(max_length=20, help_text="Name of the Catagory", blank=True, null=True)
     max_Points = models.DecimalField(max_digits=3, decimal_places=1, default=0, help_text="Maximum points for this catagory", blank=True, null=True)
-    current_Points = models.DecimalField(max_digits=5, decimal_places=2, default=0, help_text="Displaying current points. DO NOT CHANGE THIS.", blank=True, null=True)
-    
-    def get_absolute_url(self):
-      #send user to page that display the detiels of the input data
-      return reverse('survey:test-detail', kwargs={'id':self.id})
 
     def __str__(self):
-
-
-       return self.category_Name
+        return self.category_Name
 
 
 class Question(models.Model):
-   # Menu for type of questions
-   shortText = 'text'
-   longText = 'text'
-   dropdown = 'text'
-   multipleChoiceSingleOption = 'choice.label'
-   multipleChoiceMultipleOptions = 'choice.labels'
-   pictureChoiceSingleOption = 'choice.label'
-   pictureChoiceMultipleOptions = 'choice.labels'
-   email = 'email'
-   website = 'url'
-   fileUpload = 'file_url'
-   legal = 'boolean'
-   yesNo = 'boolan'
-   rating = 'number'
-   optionScale = 'number'
-   number = 'number'
-   date = 'date'
-   typeChoices = (
+    # Menu for type of questions
+    shortText = 'text'
+    longText = 'text'
+    dropdown = 'text'
+    multipleChoiceSingleOption = 'choice.label'
+    multipleChoiceMultipleOptions = 'choice.labels'
+    pictureChoiceSingleOption = 'choice.label'
+    pictureChoiceMultipleOptions = 'choice.labels'
+    email = 'email'
+    website = 'url'
+    fileUpload = 'file_url'
+    legal = 'boolean'
+    yesNo = 'boolan'
+    rating = 'number'
+    optionScale = 'number'
+    number = 'number'
+    date = 'date'
+    typeChoices = (
       (shortText, 'short_text'),
       (longText, 'long_text'),
       (dropdown, 'dropdown'),
@@ -59,28 +55,28 @@ class Question(models.Model):
       (optionScale, 'opinion_scale'),
       (number, 'number'),
       (date, 'date'),
-   )
+    )
 
-   category = models.ForeignKey(Category, related_name='question', on_delete=models.CASCADE, help_text="Choose to what Category this question belongs to" ,blank=True, null=True)
-   question_ID = models.CharField(max_length=500, unique=True, help_text="Add the Question ID from Typefrom goes here", blank=True, null=True)
-   question_Type = models.CharField(max_length=20, choices=typeChoices, help_text="Important that this is right")
-   
-   def get_absolute_url(self):
-      #send user to page that display the detiels of the input data
-      return reverse('question:detail', kwargs={'pk': self.pk})
-   
-   def __str__(self):
-      return 'QuestionID: ' + self.question_ID + ' - ' + 'Question Type ' + self.question_Type + ' - ' + 'Category:' + str(self.category)
+    category = models.ForeignKey(Category, related_name='question', on_delete=models.CASCADE, help_text="Choose to what Category this question belongs to" ,blank=True, null=True)
+    question_ID = models.CharField(max_length=500, unique=True, help_text="Add the Question ID from Typefrom goes here", blank=True, null=True)
+    question_Type = models.CharField(max_length=20, choices=typeChoices, help_text="Important that this is right")
+
+    def get_absolute_url(self):
+        # send user to page that display the detiels of the input data
+        return reverse('question:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+       return 'QuestionID: ' + self.question_ID + ' - ' + 'Question Type ' + self.question_Type + ' - ' + 'Category:' + str(self.category)
 
 
 class Answer(models.Model):
-   question = models.ForeignKey(Question, related_name='answer', on_delete=models.CASCADE,help_text="Choose to what Question the Answer belongs tobelongs to" ,blank=True, null=True)
-   the_answer = models.CharField(max_length=500, help_text="Important that this is exact", blank=True, null=True)
-   bool_answer = models.BooleanField(help_text="Important that this is exact", blank=True, null=True)
-   points = models.DecimalField(max_digits=5, decimal_places=2, default=0, help_text='points for this answer', blank=True, null=True)
+    question = models.ForeignKey(Question, related_name='answer', on_delete=models.CASCADE,help_text="Choose to what Question the Answer belongs tobelongs to" ,blank=True, null=True)
+    the_answer = models.CharField(max_length=500, help_text="Important that this is exact", blank=True, null=True)
+    bool_answer = models.BooleanField(help_text="Important that this is exact", blank=True, null=True)
+    points = models.DecimalField(max_digits=5, decimal_places=2, default=0, help_text='points for this answer', blank=True, null=True)
 
-   def __str__(self):
-      return self.the_answer
+    def __str__(self):
+       return self.the_answer
 
 
 class Response(models.Model):
